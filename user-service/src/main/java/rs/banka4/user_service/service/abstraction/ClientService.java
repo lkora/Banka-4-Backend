bcbeffc2c7c2b02a5e25365c32aa353afb7df430
LazarVuksanovic <95725910+LazarVuksanovic@users.noreply.github.com>
2025-03-02 18:23:00 +0100

    implement skeleton

--- user-service/src/main/java/rs/banka4/user_service/service/abstraction/ClientService.java
+++ user-service/src/main/java/rs/banka4/user_service/service/abstraction/ClientService.java
@@ -1,18 +1,9 @@
 package rs.banka4.user_service.service.abstraction;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import rs.banka4.user_service.dto.ClientDto;
-import rs.banka4.user_service.dto.requests.CreateClientDto;
-import rs.banka4.user_service.dto.PrivilegesDto;
-import rs.banka4.user_service.dto.requests.UpdateClientDto;
+import rs.banka4.user_service.dto.ClientResponseDto;
+import rs.banka4.user_service.dto.EmployeeResponseDto;
 
 public interface ClientService {
-    ResponseEntity<PrivilegesDto> getPrivileges(String token);
-    ResponseEntity<ClientDto> getMe(String token);
-    ResponseEntity<ClientDto> getClient(String id);
-    ResponseEntity<Void> createClient(CreateClientDto createClientDto);
-    ResponseEntity<Page<ClientDto>> getClients(String firstName, String lastName, String email, String phone, Pageable pageable);
-    ResponseEntity<Void> updateClient(String id, UpdateClientDto updateClientDto);
+    ResponseEntity<ClientResponseDto> getMe(String authorization);
 }
