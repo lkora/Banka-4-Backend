47d75aac914b2a89b99d4025b9cba73bbe0f8ecb
Arsenije <apetrovic10322rn@raf.rs>
2025-04-02 18:51:08 +0200

    stock-service: added OkHttp bean for use

---
+++ stock-service/src/main/java/rs/banka4/stock_service/config/OkHttpConfig.java
@@ -0,0 +1,14 @@
+package rs.banka4.stock_service.config;
+
+import okhttp3.OkHttpClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OkHttpConfig {
+    @Bean
+    public OkHttpClient stockHttpClient() {
+        return new OkHttpClient().newBuilder()
+                .build();
+    }
+}
